9e2d470abb4bd34e09d1b1fbfbace27190d7afca
chenchengf <chenchengf>
2024-05-06 17:49:44 +0800

    init

---
+++ tutorial/tutorial/quickstart/serializers.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.contrib.auth.models import Group, User
+from rest_framework import serializers
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url','emp_num', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
