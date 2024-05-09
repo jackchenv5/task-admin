e89a1e0e7aa6c27ba6990ad8660111c488126ddd
jackchen cc <jackchenv5@163.com>
2024-05-09 01:51:36 +0000

    delete tutorial

--- tutorial/tutorial/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('snippets.urls')),
-    path('', include('user.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-]
