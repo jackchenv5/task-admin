6b52c75a481c3e1dd21f51275998602a8ab7f672
jackchen cc <jackchenv5@163.com>
2024-05-12 15:22:11 +0000

    add

--- web/src/views/task/config/index.vue
+++ web/src/views/task/config/index.vue
@@ -1,6 +1,17 @@
 <template>
   <PageWrapper>
-    <template #headerContent> <Header /> </template>
+    <!-- <template #headerContent> <Header /> </template> -->
+    <div class="flex">
+      <Card>
+        <RadioGroup v-model:value="value1" button-style="solid">
+          <RadioButton value="1">工作流</RadioButton>
+          <RadioButton value="2">用户</RadioButton>
+          <RadioButton value="3">组</RadioButton>
+          <RadioButton value="4">角色</RadioButton>
+        </RadioGroup>
+      </Card>
+
+    </div>
       <EditRowTable/>
   </PageWrapper>
 </template>
@@ -10,6 +21,8 @@
   import Header from './components/Header.vue';
   import EditRowTable from './components/EditRowTable.vue'
 
+  import {RadioGroup,RadioButton,Card } from 'ant-design-vue';
+  const value1 = ref<string>('1');
   const loading = ref(true);
 
   setTimeout(() => {
