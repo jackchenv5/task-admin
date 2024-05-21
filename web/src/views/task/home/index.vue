299a3dcb75101ee89297cc210bac2135f5fffa1d
jackchen <33389476+jackchenv5@users.noreply.github.com>
2024-05-21 08:36:43 +0000

    useApiSelect

--- web/src/views/task/home/index.vue
+++ web/src/views/task/home/index.vue
@@ -4,24 +4,15 @@
     <!-- <div class="flex flex-col justify-center ">
       <Button size="large" danger type="primary" >下发当前任务</Button>
     </div> -->
-    <div style="width: 0%;"><TypographyTitle :level="5" ></TypographyTitle></div>
-    <div class="flex flex-1  justify-start" style="width: 100%;">
-      <TypographyTitle :level="5" class="mt-1">开始：</TypographyTitle>
+    <div style="width: 0%;"><p ></p></div>
+    <div class="flex flex-1  justify-start items-center" style="width: 100%;">
+      <p class="font-bold mt-3">开始：</p>
       <DatePicker v-model:value="value1" size="middle" style="width: 115px"/>
-      <TypographyTitle :level="5" class="mt-1 ml-4">截止：</TypographyTitle>
+      <p class="font-bold mt-3 ml-4">截止：</p>
       <DatePicker v-model:value="value4" size="middle" style="width: 115px;"/>
-      <TypographyTitle :level="5" class="mt-1 ml-4">组：</TypographyTitle>
-      <Select class=""
-      ref="select"
-      v-model:value="value2"
-      style="width: 105px"
-    >
-      <SelectOption value="jack">Jack</SelectOption>
-      <SelectOption value="lucy">Lucy</SelectOption>
-      <SelectOption value="disabled" disabled>Disabled</SelectOption>
-      <SelectOption value="Yiminghe">yiminghe</SelectOption>
-    </Select>
-    <TypographyTitle :level="5" class="mt-1 ml-4">执行者：</TypographyTitle>
+      <p class="mt-3 ml-4 font-bold">组：</p>
+      <ApiSearchSelect style="width: 115px" :api="groupListApi" result-field="items" value-field="id" label-field="name"</ApiSearchSelect>
+    <p class="mt-3 ml-4 font-bold">执行者：</p>
       <Select class=""
       ref="select"
       v-model:value="value2"
@@ -32,7 +23,7 @@
       <SelectOption value="disabled">Disabled</SelectOption>
       <SelectOption value="Yiminghe">yiminghe</SelectOption>
     </Select>
-    <TypographyTitle :level="5" class="mt-1 ml-4">状态：</TypographyTitle>
+    <p class="mt-3 ml-4 font-bold">状态：</p>
       <Select class=""
       ref="select"
       v-model:value="value2"
@@ -41,10 +32,10 @@
       <SelectOption value="jack">已发布</SelectOption>
       <SelectOption value="lucy">未发布</SelectOption>
     </Select>
-    <TypographyTitle :level="5" class="mt-1 ml-4">查询：</TypographyTitle>
+    <p class="mt-3 ml-4 font-bold">查询：</p>
     <InputSearch class=""
       v-model:value="value3" style="min-width: 200px;width: 300px"
-      placeholder="input search loading with enterButton"
+      placeholder="支持任务名、工作内容、挑战目标、任务自述"
       enter-button
     />
     </div>
@@ -52,9 +43,6 @@
   </div>
     <div class="lg:flex  mt-1" style="min-width: 1357px!important">
       <div style="width: 18%;" class="mr-2">
-        <!-- <Card title="我的工作流">
-          <p v-for="item in curJobList"><a>{{`${item.name}:${item.status_name}`}}</a></p>
-        </Card> -->
         <Card title="相关信息" class="">
           <p>执行人：陈成</p>
           <p>关联任务：</p>
@@ -97,7 +85,9 @@
   import Header from './components/Header.vue';
   import { useTaskStore } from '@/store/modules/task';
   import TaskTable from './components/TaskTable.vue';
+  import  ApiSearchSelect  from '@/components/Form/src/components/ApiSearchSelect.vue'
   import type { Dayjs } from 'dayjs';
+  import { groupListApi } from '@/api/task/group';
   type RangeValue = [Dayjs, Dayjs];
   const value1 = ref();
   const value4 = ref();
